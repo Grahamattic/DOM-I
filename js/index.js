be8a891a87e04a9e43aa416738dc3998b656216d
Shenica Graham <gshenica@gmail.com>
2021-01-13 16:11:43 -0600

    Update header navigation

--- js/index.js
+++ js/index.js
@@ -56,3 +56,34 @@ const siteContent = {
     // Update the img src for the middle image.
     middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 
+// Header Navigation
+    // Grab all of the anchor tags in the header nav bar.
+    let headerNav = document.querySelectorAll("a");
+        // Update header nav bar.
+        headerNav[0].textContent = siteContent["nav"]["nav-item-1"];
+        headerNav[1].textContent = siteContent["nav"]["nav-item-2"];
+        headerNav[2].textContent = siteContent["nav"]["nav-item-3"];
+        headerNav[3].textContent = siteContent["nav"]["nav-item-4"];
+        headerNav[4].textContent = siteContent["nav"]["nav-item-5"];
+        headerNav[5].textContent = siteContent["nav"]["nav-item-6"];
+    // Add two new header nav links.
+        // Prepend one link to the beginning of the nav list.
+        // Grab the parent element.
+        let parentElement = document.querySelector('nav');
+            // Create a new link called "Home"
+            let home = document.createElement('a');
+            home.textContent = "Home";
+            // Prepend the Home link to the beginning of the nav list.
+            parentElement.prepend(home);
+        // Append one link to the end of the nav list.
+            // Create a new link called "blog".
+            let blog = document.createElement('a');
+            blog.textContent = "Blog";
+
+            // Append the Blog link to the end of the nav list.
+            parentElement.appendChild(blog);
+            // Grab all of the anchor tags in the updated header nav bar.
+            let newHeaderNav = document.querySelectorAll("a");
+                // Change the color of the updated header nav links to green.
+                newHeaderNav.forEach(element=>{element.style.color="green"});
+
